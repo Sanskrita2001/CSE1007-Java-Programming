947ba0b071f670726b6312b3d41ca435e4ef30f5
Sanskrita Saha <60420648+Sanskrita2001@users.noreply.github.com>
2021-06-24 19:59:56 +0530

    Multiplication table till 15

---
+++ 1st Assignment/multiplication_table.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class qn5 {
+public static void main (String []args){
+for ( int i = 1 ; i<= 15 ; i++){
+for ( int j = 1 ; j<= 15 ; j++){
+System.out.print(i * j);
+System.out.print( " " );
+}
+System.out.println();
+}
+}
+}
