464fdf0f82cdd8e388d4864fabd7feffbf82a90b
Sanskrita Saha <60420648+Sanskrita2001@users.noreply.github.com>
2021-06-24 19:56:04 +0530

    operations without using third variable

---
+++ 1st Assignment/operations_without_third_variable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class operations_without_third_variable
+{
+public static void main (String[] args) {
+Scanner ob = new Scanner(System.in);
+System.out.println( "Enter two numbers:" );
+int a = ob.nextInt();
+int b = ob.nextInt();
+System.out.println( "Addition of two numbers is:" +(a+b));
+System.out.println( "Subtraction of two numbers is :" +(a-b));
+System.out.println( "Multiplication of two numbers is :" +(a*b));
+System.out.println( "Division of two numbers is:" +(a/b));
+}
+}
