80f011aae1699f4347d4a22573343cae537be497
Sanskrita Saha <60420648+Sanskrita2001@users.noreply.github.com>
2021-06-24 20:01:56 +0530

    ASCII Table in java

---
+++ 1st Assignment/ASCII_Table.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ASCII_Table {
+public static void main (String []args){
+System.out.println( "For 'A' to 'Z' :" );
+for ( int i = 'A' ; i<= 'Z' ; i++){
+System.out.println(( char )(i)+ "-" +i+ " " );
+}
+System.out.println( "For 'a' to 'z' :" );
+for ( int i = 'a' ; i<= 'z' ; i++){
+System.out.println(( char )(i)+ "-" +i+ " " );
+}
+System.out.println( "For '0' to '9' :" );
+for ( int i = '0' ; i<= '9' ; i++){
+System.out.println(( char )(i)+ "-" +i+ " " );
+}
+}
+}
